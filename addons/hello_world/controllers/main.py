4a35094a3ad7ceb41ccb5e3fa40cd14b9da60ea9
nguyenpnt <nguyenbv147@gmail.com>
2026-01-21 15:17:59 +0700

    chore: remove hello_world module

--- addons/hello_world/controllers/main.py
+++
@@ -1,9 +0,0 @@
-from odoo import http
-from odoo.http import request
-
-
-class HelloWorldController(http.Controller):
-
-    @http.route('/hello', auth='public', website=True)
-    def hello_world(self, **kw):
-        return "<h1>Hello World</h1>"
